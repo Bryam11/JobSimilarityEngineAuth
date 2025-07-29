bc5e10adac7a541ddeac23fa3c41a9f80a4b259c
Bryam Chuchuca <xavierchuchuca18@gmail.com>
2025-07-29 15:57:57 -0500

    encrypt information

--- src/main/java/com/tecazuay/auth/application/dto/LoginRequestDto.java
+++ src/main/java/com/tecazuay/auth/application/dto/LoginRequestDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class LoginRequestDto {
     @NotBlank(message = "Email is required")
-    @Email(message = "Email must be valid")
+    //@Email(message = "Email must be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
